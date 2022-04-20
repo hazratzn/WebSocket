64b6e8cdc9d76e0e85db9f71ee46422d21145670
hazratzn <hazratzn@code.edu.az>
2022-04-20 07:33:50 +0400

    WebSocket

---
+++ WebSocket/WebsocketTest/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebsocketTest.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage", username, message);
+        }
+    }
+}
---
+++ WebSocket/WebsocketTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebsocketTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
